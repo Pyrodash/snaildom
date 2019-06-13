2a340fd7462033cfba78ee37fe53b19d000e8428
Jack <ahmogo@yahoo.com>
2019-06-13 02:42:46 +0200

    Performance improvement for the latest fix.

--- Snaildom/Dependencies/Shell.js
+++ Snaildom/Dependencies/Shell.js
@@ -1,32 +1,39 @@
 'use strict';
 
-const Dependency = require('../Dependency');
-const Shell      = require('../Shell');
+const Dependency  = require('../Dependency');
+const Shell       = require('../Shell');
+
+const defaultType = 'yellow';
 
 class MyShell extends Dependency {
-  createShell() {
+  createShell(shellObj) {
     return new Promise((resolve, reject) => {
-      this.database.getShell('Owner', this.id).then(shell => {
-        if(shell) {
-          shell = {
-            id: shell.ID,
-            owner: this,
-            furniture: shell.Furniture,
-            type: shell.Type
-          };
-          shell = new Shell(shell, this.world);
-          this.myShell = shell;
+      if(!shellObj) {
+        this.database.getShell('Owner', this.id).then(shell => {
+          if(shell)
+              this.createShell(shell).then(resolve).catch(reject);
+          else
+            this.database.createShell({
+              Owner: this.id,
+              Type: defaultType
+            })
+            .then(res => {
+              this.createShell({ ID: res[0], Type: defaultType }).then(resolve).catch(reject);
+            })
+            .catch(this.logger.error);
+        }).catch(this.logger.error);
+      } else {
+        var shell = {
+          id: shellObj.ID,
+          owner: this,
+          furniture: shellObj.Furniture || '',
+          type: shellObj.Type
+        };
+        shell = new Shell(shell, this.world);
+        this.myShell = shell;
 
-          resolve(shell);
-        } else
-          this.database.createShell({
-            Owner: this.id
-          })
-          .then(res => {
-            this.createShell().then(resolve).catch(reject);
-          })
-          .catch(this.logger.error);
-      }).catch(this.logger.error);
+        resolve(shell);
+      }
     });
   }
 }
